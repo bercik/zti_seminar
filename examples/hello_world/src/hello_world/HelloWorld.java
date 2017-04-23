e12b4a9b0899245957b5a3eac87c23f6582b57f9
Robert <robert.cebula1@gmail.com>
2017-04-23 14:43:45 +0200

    Add example and presentation

---
+++ examples/hello_world/src/hello_world/HelloWorld.java
@@ -0,0 +1,14 @@
+package hello_world;
+
+public class HelloWorld
+{
+    public void sayHello()
+    {
+        System.out.print(" Hello ");
+    }
+
+    public static void main(String[] args)
+    {
+        new HelloWorld().sayHello();
+    }
+}
---
+++ examples/hello_world/src/hello_world/World.aj
@@ -0,0 +1,8 @@
+package hello_world;
+
+public aspect World 
+{
+    pointcut greeting() : call(* HelloWorld.sayHello(..));
+    
+    after() returning : greeting() { System.out.print("World!"); }
+}
